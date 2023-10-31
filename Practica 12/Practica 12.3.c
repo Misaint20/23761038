45b6f84a44c7404e5b4afb4adbfd355fe183c038
Misaint20 <zmisaintm@gmail.com>
2023-10-30 17:11:39 -0700

    Programacion1SB

--- Practica 12/Practica 12.3.c
+++ Practica 12/Practica 12.3.c
@@ -14,8 +14,8 @@ int main()
     scanf("%f, %f, %f",&A,&B,&C);
 
     if (B*B-4*A*C > 0 && 2*A != 0) {
-        X1=(-B+(sqrt(B*B-4*A*C)))/2*A;
-        X2=(-B-(sqrt(B*B-4*A*C)))/2*A;
+        X1=(-B+(sqrt(B*B-4*A*C)))/(2*A);
+        X2=(-B-(sqrt(B*B-4*A*C)))/(2*A);
         printf("El valor de X1 es: %0.2f, y el valor de X2 es: %0.2f",X1,X2);
     }  else {
         printf("No es posible calcular la expresion");
