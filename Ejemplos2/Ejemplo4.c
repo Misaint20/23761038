6092bd9bd7b9fa8fcd53749a8b999f1e32ab84e4
Misaint20 <zmisaintm@gmail.com>
2023-11-08 15:19:26 -0800

    Programacion1SB

---
+++ Ejemplos2/Ejemplo4.c
@@ -0,0 +1,27 @@
+/*
+GASTO
+Calcular los gastos totales de un viaje
+Autor: Zahid Misaint Murillo Valenzuela
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+float GT, GASTO;
+
+int main()
+{
+    printf("Dame el gasto: ");
+    scanf("%f",&GASTO);
+
+    while (GASTO != -1)
+    {
+        GT = GT + GASTO;
+        printf("Dame el siguiente gasto: ");
+        scanf("%f",&GASTO);
+    }
+
+    printf("El total de gastos fue de $%0.2f",GT);
+
+    return 0;
+}
