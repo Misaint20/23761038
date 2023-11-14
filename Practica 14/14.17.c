c8a1f78fa274035d104bd4610122304b6dd2e0a4
Misaint20 <zmisaintm@gmail.com>
2023-11-13 17:00:47 -0800

    Programacion1SB

---
+++ Practica 14/14.17.c
@@ -0,0 +1,15 @@
+/*
+NUMEROS PRIMOS
+Obtener y contar los numeros primos menores a M
+Autor: Zahid Misaint Murillo Valenzuela
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int N, M;
+
+int main() {
+    
+}
