0621006224e9e72d859fbf98075ed5f2e643d928
Misaint20 <zmisaintm@gmail.com>
2023-11-14 20:47:50 -0800

    Programacion1SB

--- Practica 14/14.17.c
+++ Practica 14/14.17.c
@@ -8,8 +8,21 @@ Autor: Zahid Misaint Murillo Valenzuela
 #include <stdlib.h>
 #include <math.h>
 
-int N, M;
+int N, M, i=2, CP;
 
 int main() {
+
+    printf("Dame el numero a considerar: ");
+    scanf("%i",&M);
+
+    for (i = 2; i < M; i++)
+    {
+        for ( i = 0; i < M; i++)
+        {
+            
+        }
+        
+    }
     
+    return 0;
 }
