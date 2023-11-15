5c628d292a064bf93fb0fdacdcb99c43da494aff
Misaint20 <zmisaintm@gmail.com>
2023-11-15 15:07:03 -0800

    Programacion1SB

--- Practica 14/14.17.c
+++ Practica 14/14.17.c
@@ -8,21 +8,35 @@ Autor: Zahid Misaint Murillo Valenzuela
 #include <stdlib.h>
 #include <math.h>
 
-int N, M, i=2, CP;
+int N, M, i=2, CP=1, j, c;
 
 int main() {
 
     printf("Dame el numero a considerar: ");
     scanf("%i",&M);
+    
+    CP = 1;
+    printf("%i, ",CP);
+    CP = CP + 1;
 
     for (i = 2; i < M; i++)
     {
-        for ( i = 0; i < M; i++)
+        c=0;
+        for ( j = 2; j < i; j++)
         {
-            
+            if (i%j == 0)
+            {
+                c = c + 1;
+            }
+        }
+        if (c == 0)
+        {
+            printf("%i, ",i);
+            CP = CP + 1;
         }
         
     }
+    printf("\nLa cantidad de numeros primos es de %i",CP);
     
     return 0;
 }
